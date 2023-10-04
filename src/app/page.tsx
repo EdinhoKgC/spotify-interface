28dda049d3560106618d4da2398d6457edd08417
Edilson Torres <edilson.td@outlook.com>
2023-10-03 23:50:31 -0300

    second commit with partial interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,168 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  Home as HomeIcon,
+  Library,
+  Play,
+  Search,
+} from "lucide-react";
+import Image from "next/image";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64 bg-zinc-950 p-6">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+          </div>
+          <nav className="space-y-5 mt-10">
+            <a
+              href="#"
+              className="flex items-center gap-2 text-sm font-semibold text-zinc-200"
+            >
+              <HomeIcon />
+              Home
+            </a>
+            <a
+              href="#"
+              className="flex items-center gap-2 text-sm font-semibold text-zinc-200"
+            >
+              <Search />
+              Search
+            </a>
+            <a
+              href="#"
+              className="flex items-center gap-2 text-sm font-semibold text-zinc-200"
+            >
+              <Library />
+              Your Library
+            </a>
+          </nav>
+          <nav className="mt-6 pt-6  border-t border-zinc-800 flex flex-col gap-3">
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Trap Brasil
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Top Brasil
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Hard Training
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Eletrônicas
+            </a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+          </div>
+          <h1 className="text-3xl font-semibold mt-10">Good afternoon</h1>
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.png"
+                width={104}
+                height={104}
+                alt="Capa do album Californication"
+              />
+              <strong>Californication</strong>
+              <button className="w-12 h-12 pl-1 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.png"
+                width={104}
+                height={104}
+                alt="Capa do album Californication"
+              />
+              <strong>Californication</strong>
+              <button className="w-12 h-12 pl-1 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.png"
+                width={104}
+                height={104}
+                alt="Capa do album Californication"
+              />
+              <strong>Californication</strong>
+              <button className="w-12 h-12 pl-1 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.png"
+                width={104}
+                height={104}
+                alt="Capa do album Californication"
+              />
+              <strong>Californication</strong>
+              <button className="w-12 h-12 pl-1 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.png"
+                width={104}
+                height={104}
+                alt="Capa do album Californication"
+              />
+              <strong>Californication</strong>
+              <button className="w-12 h-12 pl-1 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.png"
+                width={104}
+                height={104}
+                alt="Capa do album Californication"
+              />
+              <strong>Californication</strong>
+              <button className="w-12 h-12 pl-1 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+          </div>
+        </main>
+      </div>
+      <footer className="bg-zinc-900 border-t border-zinc-700 p-6">
+        footer
+      </footer>
+    </div>
+  );
 }
